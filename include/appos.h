637e94f05ea9f8c60fe3fcd93433ce41e280e40f
Andreu Santaren Llop <andreu.santaren@gmail.com>
2016-10-15 08:54:14 +0200

    Use GCC definitions instead of internal ones.

--- include/appos.h
+++ include/appos.h
@@ -1,24 +1,25 @@
 #ifndef APPOS_H
 #define	APPOS_H
 
-#ifndef NULL
-#define NULL                (void *)0
-#endif
+// GCC defined types
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #ifndef YES
-#define YES                 (1==1)
+#define YES                 true
 #endif
 
 #ifndef NO
-#define NO                  (!YES)
+#define NO                  false
 #endif
 
 #ifndef TRUE
-#define TRUE                YES
+#define TRUE                true
 #endif
 
 #ifndef FALSE
-#define FALSE               NO
+#define FALSE               false
 #endif
 
 #ifndef REBOOT_ON_FATAL
@@ -46,22 +47,7 @@ typedef unsigned int            TASK;
 typedef unsigned int            PRIORITY;
 typedef unsigned char           EVENT;
 typedef unsigned long long      TIME;
-
-// TODO: define this on an arch header
-typedef unsigned char           uint8_t;
-typedef signed char             int8_t;
-typedef unsigned short int      uint16_t;
-typedef signed short int        int16_t;
-typedef unsigned int            uint32_t;
-typedef signed int              int32_t;
-typedef unsigned long long      uint64_t;
-typedef signed long long        int64_t;
-typedef unsigned long           size_t;
 typedef long                    time_t;
-
-#ifndef __cplusplus
-typedef char                    bool;
-#endif
 
 #endif
 
